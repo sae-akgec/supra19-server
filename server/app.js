b1a08b06910126eede45be6e55cd632530f7ab10
Rishabh2324 <rishabh2401jain@gmail.com>
2019-07-06 03:24:46 +0530

    seconr2

--- server/app.js
+++ server/app.js
@@ -47,6 +47,6 @@ app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '../client/dist/index.html'));
 });
 
-app.listen(port, '0.0.0.0', ()=>{
+app.listen(port, ()=>{
     console.log('Server is started at port : ' + port);
 });
